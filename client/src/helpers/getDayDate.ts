4fbe9ee56deebb23d03b9e6eca647399d3520692
Maksim Smetanin <maks.s923@mail.ru>
2023-03-16 18:02:34 +0500

    fix: displayed date

--- client/src/helpers/getDayDate.ts
+++ client/src/helpers/getDayDate.ts
@@ -2,7 +2,15 @@ import moment from "moment";
 
 export function getDayDate(offset: number) {
   const startDayWeek = new Date(moment().startOf('isoWeek').toDate())
-  const currentDate = new Date(startDayWeek.getFullYear(), startDayWeek.getMonth(), startDayWeek.getDate() + offset)
+
+  // Если суббота или вск, то показываем даты на следующую неделю
+  const todayDay = moment().toDate().getDay()
+  let currentOffset = 0 + offset
+  if(todayDay === 0 || todayDay === 6) {
+    currentOffset = 7 + offset
+  }
+
+  const currentDate = new Date(startDayWeek.getFullYear(), startDayWeek.getMonth(), startDayWeek.getDate() + currentOffset)
   return Intl.DateTimeFormat('ru', {
     day: 'numeric',
     month: 'short'
